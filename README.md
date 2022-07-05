e01eb5e885d7c855dffba22dbc0f36c7d4c0d4e5
Sox-I <pan1204320289@gmail.com>
2022-07-05 14:06:56 +0800

    index the holders' addresses

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # subgraph
-# subgraph
--- src/contract.ts
+++ src/contract.ts
@@ -1,67 +1,54 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, Bytes } from "@graphprotocol/graph-ts"
 import {
   Contract,
-  Approval,
-  ApprovalForAll,
   NewEpicNFTMinted,
   Transfer
 } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { Square, Item } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+export function handleNewEpicNFTMinted(event: NewEpicNFTMinted): void {
+  let square = Square.load("0");
+  if(square === null) {
+    square = new Square("0");
+    square.holderNum = new BigInt(0);
+    square.items = new Array<string>(0);
+    square.holderAddress = new Array<Bytes>(0);
   }
+  
+  let item = Item.load(event.params.tokenId.toString());
+  if(item === null) {
+  item = new Item(event.params.tokenId.toString());
+  item.owner = event.params.sender;
+  }
+  
+  item.save();
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balanceOf(...)
-  // - contract.getApproved(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.name(...)
-  // - contract.ownerOf(...)
-  // - contract.pickRandomFirstWord(...)
-  // - contract.pickRandomSecondWord(...)
-  // - contract.pickRandomThirdWord(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenURI(...)
+  let holderAddress = changetype<Bytes[]>(square.holderAddress);
+  if(holderAddress.includes(event.params.sender)===false) {
+    holderAddress.push(event.params.sender);
+    square.holderAddress = holderAddress;
+    square.holderNum = new BigInt(holderAddress.length);
+  }
+  
+  let items = changetype<string[]>(square.items);
+  items.push(event.params.tokenId.toString());
+  square.items = items;
+  square.save();
 }
 
-export function handleApprovalForAll(event: ApprovalForAll): void {}
-
-export function handleNewEpicNFTMinted(event: NewEpicNFTMinted): void {}
-
 export function handleTransfer(event: Transfer): void {}
+
+
+// export function handleNewEpicNFTMinted(event: NewEpicNFTMinted): void {
+//   let minted = Square.load(event.transaction.hash.toHex());
+//   if(minted === null) {
+//     minted = new Square(event.transaction.hash.toHex());
+//   }
+  
+//   minted.itemId = event.params.tokenId;
+//   minted.owner = event.params.sender;
+//   minted.save();
+// }
+
+// export function handleTransfer(event: Transfer): void {}
